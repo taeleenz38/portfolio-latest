debb5721974f7c10e11406d91ed83fa6d1970325
Tae Kyu Lee <taekyu.lee3@gmail.com>
2025-02-02 18:14:23 +1100

    navbar updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-primary h-screen w-screen">
-      <h1 className="text-white pl-10 pt-10 text-xl font-bold">Hi Ivan</h1>
+      <h1 className="text-white pl-10 pt-10 text-xl font-bold"></h1>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center py-8 px-12 text-accent">
+    <div className="fixed top-0 left-0 w-full z-50 flex justify-between items-center py-8 px-12 text-accent">
       <div>LOGO</div>
       <div className="flex items-center gap-10 text-sm">
         <a href="" className="hover:text-secondary duration-150 font-medium ">
@@ -17,7 +17,7 @@ const Navbar = () => {
         <a href="" className="hover:text-secondary duration-150 font-medium ">
           <span className="text-white">04.</span> Contact
         </a>
-        <button className="text-white font-medium border-[1px] rounded-md px-4 py-2 transition-transform hover:translate-x-[-3px] hover:translate-y-[-3px] hover:shadow-[2px_2px_0px_0px] duration-300">
+        <button className="text-white font-medium border-[1px] rounded-md px-4 py-2 hover:translate-x-[-3px] hover:translate-y-[-3px] hover:shadow-[3px_3px_0px_rgba(255,255,255)] duration-300">
           Resume
         </button>
       </div>
